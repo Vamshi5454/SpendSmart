07fab42d50040f1af824dbb93c6c9151281e4dd2
Vamshi5454 <ponugotivamshi007@gmail.com>
2024-11-07 23:12:12 -0600

    Header Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import Header from "./components/Header/Header.js";
 
 function App() {
   return (
     <div className="App">
-      <h1>This the client side</h1>
-      <Home name="vamshi" />
+      {/* <h1>This the client side</h1> */}
+      {/* <Home name="vamshi" /> */}
+      <Header />
     </div>
   );
 }
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,10 +2,17 @@ import React from "react";
 import "../Header/Header.css";
 
 function Header() {
+  // const  handleLogOut=()=>{
+  //    route to Home page
+  // }
+
   return (
     <div className="header">
-      <h1>Spend Smart</h1>
-      <p>Track and Control your Expense</p>
+      <div>
+        <h1>Spend Smart</h1>
+        <p>Track and Control your Expense</p>
+      </div>
+      <button>Log Out</button>
     </div>
   );
 }

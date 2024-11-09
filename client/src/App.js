5415a6df933ade1894ad6b88d97cf8c96806a8eb
Vamshi5454 <ponugotivamshi007@gmail.com>
2024-11-08 23:21:55 -0600

    Dashboard added

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header.js";
-
+import DashBoard from "./components/DashBoard/DashBoard.js";
 function App() {
   return (
     <div className="App">
       {/* <h1>This the client side</h1> */}
       {/* <Home name="vamshi" /> */}
       <Header />
+      <DashBoard />
     </div>
   );
 }
--- client/src/components/DashBoard/DashBoard.js
+++ client/src/components/DashBoard/DashBoard.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./DashBoard.css";
 
 function DashBoard() {
-  return <div></div>;
+  const [income, setIncome] = useState("");
+
+  const handleIncome = (e) => {
+    setIncome(e.target.value);
+  };
+
+  return (
+    <div>
+      <form>
+        <label>Income:</label>
+        <input
+          type="number"
+          value={income}
+          onChange={handleIncome}
+          placeholder="Enter Your Income"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default DashBoard;

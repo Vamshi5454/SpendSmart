45bb1cc07d9f167ec54916c5c2742a4b6c0e7292
Vamshi5454 <ponugotivamshi007@gmail.com>
2024-11-10 12:00:00 -0600

    loin and register pages added

--- client/src/components/Expenses/Expenses.js
+++ client/src/components/Expenses/Expenses.js
@@ -1,17 +1,36 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+
+// function Expenses({ expenses }) {
+//   console.log(expenses);
+//   return (
+//     <div>
+//       These are the expenses
+//       {/* {expenses[0]} */}
+//       {expenses.map((item, index) => {
+//         <div key={index}>
+//           {/* <p>Expense:{item[0].expense}</p> */}
+//           <p>Amount: {item.amount}</p>
+//         </div>;
+//       })}
+//     </div>
+//   );
+// }
+
+// export default Expenses;
+
+import React from "react";
 
 function Expenses({ expenses }) {
   console.log(expenses);
   return (
     <div>
       These are the expenses
-      {/* {expenses[0]} */}
-      {expenses.map((item, index) => {
-        <div key={index}>
-          <p>Expense:{item.expense}</p>
+      {expenses.map((item, index) => (
+        <li key={index}>
+          <p>Expense: {item.expense}</p>
           <p>Amount: {item.amount}</p>
-        </div>;
-      })}
+        </li>
+      ))}
     </div>
   );
 }

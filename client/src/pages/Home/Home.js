cbf9492b96c8f90684b86863383eb7367847e9e3
Vamshi5454 <ponugotivamshi007@gmail.com>
2024-11-18 22:57:02 -0600

    updated

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashBoard from "../../components/DashBoard/DashBoard";
+import axios from "axios";
 
 function Home() {
-  const location = useLocation();
-  const { userData } = location.state || {};
-  console.log(userData);
+  const [name, setName] = useState("");
+  // const location = useLocation();
+  // const { userData } = location.state || {};
+  // conso(le.log(userData);
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:3001/auth/find")
+      .then((response) => setName(response.data.username))
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
   return (
     <div>
-      <h1>Welcome :{userData.username}</h1>
+      <h1>Welcome {name}</h1>
       {/* <h1> {username.id}</h1> */}
-      <DashBoard id={userData.id} />
+      <DashBoard />
     </div>
   );
 }
